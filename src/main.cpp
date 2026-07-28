6795b90737f7cc64449cc0310dc8ed8d4ddda5f9
triyog-shrestha <triyogshrestha86@gmail.com>
2026-07-28 23:58:18 +0545

    added food class

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,16 @@ Color grass = {63, 172, 25, 255};
 float cellsize = 30;
 float cellcount = 25;
 
+class Food{
+    public:
+    Vector2 position = {5,6};
 
+    void draw(){
+        DrawRectangle(position.x*cellsize, position.y*cellsize, cellsize, cellsize, RED);
+    }
+};
+
+Food food;
 
 int main(){
     InitWindow(cellsize*cellcount, cellsize*cellcount, "Snake"); 
@@ -15,6 +24,8 @@ int main(){
     while (WindowShouldClose() == false) {
         BeginDrawing();
         ClearBackground(grass);
+
+        food.draw();
 
         EndDrawing();
     }
