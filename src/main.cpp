a360d0d89879209abf8740e9614355cf51ec295b
triyog-shrestha <triyogshrestha86@gmail.com>
2026-07-28 23:50:24 +0545

    added cell divisions

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,20 @@
 #include<raylib.h>
 
 Color grass = {63, 172, 25, 255};
+float cellsize = 30;
+float cellcount = 25;
 
-class Snake{
-    public:
-        float posX, posY, width, height, speed;
 
-        void drawSnake(){
-            DrawRectangle(posX, posY, width, height, WHITE);
-        }
-};
-
-Snake snake;
 
 int main(){
-    int winW = 1000;
-    int winH = 1000;
-    InitWindow(winW, winH, "Snake"); 
+    InitWindow(cellsize*cellcount, cellsize*cellcount, "Snake"); 
     SetTargetFPS(60);
 
-
-    snake.posX = winW/2.0;
-    snake.posY = winH/2.0;
-    snake.width = 100;
-    snake.height = 20;
-    snake.speed = 10;
 
 
     while (WindowShouldClose() == false) {
         BeginDrawing();
         ClearBackground(grass);
-        snake.drawSnake();
-        snake.posX += snake.speed;
-
-        
 
         EndDrawing();
     }
